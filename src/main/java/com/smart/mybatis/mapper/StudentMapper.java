fd86bf4560a37f1ebc843baa66a6812f4a3c5530
guanfenghua <283413589@qq.com>
2019-12-13 09:51:26 +0800

    基础架构-删除无用代码

--- src/main/java/com/smart/mybatis/mapper/StudentMapper.java
+++
@@ -1,7 +0,0 @@
-package com.smart.mybatis.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface StudentMapper {
-}
--- src/main/java/com/smart/mybatis/service/TeacherService.java
+++
@@ -1,9 +0,0 @@
-package com.smart.mybatis.service;
-
-import com.smart.mybatis.model.Teacher;
-import com.smart.mybatis.service.impl.BaseServiceImpl;
-import org.springframework.stereotype.Service;
-
-@Service
-public class TeacherService extends BaseServiceImpl<Teacher>{
-}
